8e6f396b1a0c71e16fc8daad414040de905a62d3
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-08-10 13:26:40 +0530

    Remove unnecessary lambdas.

--- universe.py
+++ universe.py
@@ -16,23 +16,26 @@ class Universe:
         return self.rules == other.rules and self.life == other.life
 
     def __str__(self):
-        return "Universe{{{0}\n[{1}]}}".format(str(self.rules), ", ".join(map(lambda it: str(it), self.life)))
+        return "Universe{{{0}\n[{1}]}}".format(str(self.rules), ", ".join(map(str, self.life)))
 
     def surviving_cells(self) -> set:
-        return set(filter(lambda cell: self.cell_survives(cell), self.life))
+        return set(filter(self.cell_survives, self.life))
 
     def dead_neighbors_of_living_cells(self) -> set:
         return set([
             dead_neighbor
-            for dead_neighbors in map(lambda live_cell: self.cell_dead_neighbors(live_cell), self.life)
+            for dead_neighbors in map(self.cell_dead_neighbors, self.life)
             for dead_neighbor in dead_neighbors
         ])
 
     def born_cells(self) -> set:
-        return set(filter(lambda cell: self.cell_born(cell), self.dead_neighbors_of_living_cells()))
+        return set(filter(self.cell_born, self.dead_neighbors_of_living_cells()))
 
     def cell_is_alive(self, cell):
         return cell in self.life
+
+    def cell_is_dead(self, cell):
+        return cell not in self.life
 
     def cell_survives(self, cell):
         return self.rules.survives(self.cell_count_live_neighbors(cell))
@@ -41,10 +44,10 @@ class Universe:
         return self.rules.born(self.cell_count_live_neighbors(cell))
 
     def cell_dead_neighbors(self, cell: point) -> set:
-        return set(filter(lambda it: not self.cell_is_alive(it), cell.neighbors()))
+        return set(filter(self.cell_is_dead, cell.neighbors()))
 
     def cell_live_neighbors(self, cell: point) -> set:
-        return set(filter(lambda it: self.cell_is_alive(it), cell.neighbors()))
+        return set(filter(self.cell_is_alive, cell.neighbors()))
 
     def cell_count_live_neighbors(self, cell):
         return len(self.cell_live_neighbors(cell))
